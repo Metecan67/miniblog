3187d71aee59dc6b0a49c6669a66e7d0a93435c4
Metecan67 <gunsan1905@gmail.com>
2024-05-07 14:13:18 +0300

    miniblog

---
+++ miniblog/lib/blocs/articles/article_state.dart
@@ -0,0 +1,15 @@
+import 'package:miniblog/models/blog.dart';
+
+abstract class ArticleState {}
+
+class ArticlesNotLoaded extends ArticleState {}
+
+class ArticlesLoaded extends ArticleState {
+  final List<Blog> blogs;
+
+  ArticlesLoaded({required this.blogs});
+}
+
+class ArticlesLoading extends ArticleState {}
+
+class ArticlesLoadFail extends ArticleState {}
---
+++ miniblog/lib/screens/homepage.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:miniblog/screens/add_blog.dart';
+import 'package:miniblog/screens/blog_list.dart';
+import 'package:miniblog/themes/dark_theme.dart';
+import 'package:miniblog/themes/light_theme.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+class Homepage extends StatefulWidget {
+  const Homepage({Key? key}) : super(key: key);
+
+  @override
+  _HomepageState createState() => _HomepageState();
+}
+
+class _HomepageState extends State<Homepage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Ana Sayfa"),
+        actions: [
+          IconButton(
+              onPressed: () {
+                Navigator.of(context)
+                    .push(MaterialPageRoute(builder: (ctx) => AddBlog()));
+              },
+              icon: const Icon(Icons.add))
+        ],
+      ),
+      body: const BlogList(),
+    );
+  }
+}
